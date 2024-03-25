45f52f6bc5bba7df992d31f3dc697910a783a8f3
akash kennedy <akashkennedy1@gmail.com>
2024-03-25 10:24:14 +0530

    Final Commit

--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,7 +1,35 @@
+import { offer } from "../assets/images";
+import Button from "../components/Button";
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
-  )
-}
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+      <div className="flex-1">
+        <img
+          src={offer}
+          width={773} 
+          height={687}  
+          className="object-contain w-full"
+        />
+      </div>
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin  text-4xl capitalize font-bold lg:max-w-lg">
+          <span className="text-coral-red">Special </span>Offer
+         </h2>
+         <p className="mt-4 max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals, From premier selctions to incredible savings, we offer unparaelleled value that sets us apart.</p>
+         <p className="mt-6 lg:max-w-lg info-text">
+          Navigate a realm of possiblities designed to fullfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional. 
+         </p>
+         <div className='mt-11 flex flex-wrap gap-4'>
+         <Button label="View Details" />
+         <Button 
+         label='Learn More' 
+         backgroundColor='bg-white' borderColor='border-slate-gray' text-color='text-slate-gray'
+         />
+         </div>
+         </div>
+    </section>
+  );
+};
 
-export default SpecialOffer
+export default SpecialOffer;
